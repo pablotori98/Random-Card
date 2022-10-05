b1c581fcdb14449f5fb3786906b221d59dd35dcd
pablotori98 <pablotori98@gmail.com>
2022-10-05 19:01:50 +0000

    Primera entrega

--- src/app.js
+++ src/app.js
@@ -9,3 +9,23 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const palos = ["❤️", "♠️", "☘️", "♦️"];
+
+function getElementsRandom(array) {
+  const numeroAleatorio = Math.floor(Math.random() * array.length);
+  const elementoAleatorio = array[numeroAleatorio];
+  return elementoAleatorio;
+}
+
+const palosRandom = getElementsRandom(palos);
+
+const cardNumbers = document.querySelector("#cardnumber");
+cardNumbers.innerHTML = getElementsRandom(number);
+
+const cardPalos = document.querySelector("#palo");
+cardPalos.innerHTML = palosRandom;
+
+const palo2 = document.querySelector("#palo2");
+palo2.innerHTML = palosRandom;
